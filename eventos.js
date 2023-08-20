51cb7690967ddadcd67dc43128633380242ad36a
Jmessano <joaco2551994@gmail.com>
2023-08-20 20:09:30 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -1,7 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
     const divBoton = document.getElementById("divContenedor");
+    const boton = document.getElementById("saludar");
 
     divBoton.addEventListener("click", function () {
         alert("Hola! Soy el div");
     });
+
+    boton.addEventListener("click", function (event) {
+        event.stopPropagation(); // Evita la propagación del evento al div
+        saludar(); // Llama a la función saludar
+    });
+
+    function saludar() {
+        alert("Hola");
+    }
 });
